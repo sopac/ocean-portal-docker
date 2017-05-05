1849d6e3a65f4b092a5d6c75e6fc6f8c697e096b
Sachindra Singh <sachindras@spc.int>
2017-05-05 14:44:10 +1200

    setup.sh typo fixed.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-docker exec -it oceanportal_oceanportal_1 bash -c "cd /ocean-portal &&  python setup.py install --root=/opt/ocean-portal/"
+docker exec -it oceanportaldocker_oceanportal_1 bash -c "cd /ocean-portal &&  python setup.py install --root=/opt/ocean-portal/"
 docker cp js/comp/datepick oceanportaldocker_oceanportal_1:/opt/ocean-portal/usr/local/share/portal/js/comp/
 docker exec -it oceanportaldocker_oceanportal_1 bash -c "chmod 777 -R /opt/data/"
 
