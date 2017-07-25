79d0e2c86d3a6a5c65924bbb362ee73eeaceff04
Sachindra Singh <sachindras@spc.int>
2017-07-25 14:27:41 +1200

    Start and Stop scripts on docker-compose

--- stop.sh
+++ stop.sh
@@ -1,2 +1,3 @@
-docker stop oceanportaldocker_oceanportal_1
-docker rm oceanportaldocker_oceanportal_1
+docker-compose stop
+#docker stop oceanportaldocker_oceanportal_1
+#docker rm oceanportaldocker_oceanportal_1
